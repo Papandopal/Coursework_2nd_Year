5e0a1cb96ea29450bcd457796a73fedb83e42e00
Papandopal <168136761+Papandopal@users.noreply.github.com>
2025-04-11 17:01:50 +0300

    Add big map and centrated body
    Architeche is also "govno"

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Net.WebSockets;
 using System.Numerics;
 using System.Text;
@@ -15,6 +15,8 @@ record PosUpdate
     public double size { get; set; }
     public double speed { get; set; } 
     public String type { get; set; } = "pos";
+    public double mouse_x { get; set; }
+    public double mouse_y { get; set; }
 };
 
 public class HomeController : Controller
@@ -25,8 +27,6 @@ public class HomeController : Controller
     static int userGen = 0;
 
     private readonly ILogger<HomeController> _logger;
-
-    private double mouse_x = -1, mouse_y = -1;
 
     public HomeController(ILogger<HomeController> logger)
     {
@@ -84,20 +84,23 @@ public class HomeController : Controller
 
     private async Task Echo(WebSocket webSocket)
     {
-        var rand = new Random();
         PosUpdate curPos = new PosUpdate();
-        curPos.x = rand.Next(0, 1200);
+        curPos.x = GetRandCoord();
         curPos.size = 20;
-        curPos.y = rand.Next(0, 600);
+        curPos.y = GetRandCoord();
         curPos.user = userGen++;
         curPos.speed = 3;
-
-        await UpdateAll();
+        curPos.mouse_x = -1;
+        curPos.mouse_y = -1;
 
         var time_call_back = new TimerCallback(GoToMouseCoord);
-        var timer = new Timer(time_call_back, curPos, 1000, 20);
+        var timer = new Timer(time_call_back, (curPos, webSocket), 1000, 20);
 
         posUpdates.Add(curPos);
+        
+        await UpdateAll();
+
+        await UpdateCurrentUserScreen(curPos, webSocket);
 
         var buffer = new byte[1024 * 4];
         var receiveResult = await webSocket.ReceiveAsync(
@@ -111,20 +114,18 @@ public class HomeController : Controller
             var data = ss.Split();
 
             if (data.First() == "move")
-            { 
-                mouse_x = double.Parse(data.ElementAt(1).Remove(0, 2));
-                mouse_y = double.Parse(data.ElementAt(2).Remove(0, 2));
+            {
+                string data_x = data.ElementAt(1).Remove(0, 2);
+                string data_y = data.ElementAt(2).Remove(0, 2);
+                data_x = data_x.Replace('.', ',');
+                data_y = data_y.Replace(".", ",");
+                curPos.mouse_x = double.Parse(data_x);
+                curPos.mouse_y = double.Parse(data_y);
+                Console.WriteLine(data_x);
+                Console.WriteLine(data_y);
                 
             }
 
-            /*
-            if (ss == "moveW") curPos.y -= 5;
-            if (ss == "moveS") curPos.y += 5;
-            if (ss == "moveA") curPos.x -= 5;
-            if (ss == "moveD") curPos.x += 5;
-            if (ss == "move+") curPos.size -= 5;
-            if (ss == "move-") curPos.size += 5;
-            */
             await UpdateAll();
 
             receiveResult = await webSocket.ReceiveAsync(
@@ -139,8 +140,17 @@ public class HomeController : Controller
 
     async void GoToMouseCoord(object obj)
     {
-        PosUpdate curPos = (PosUpdate)obj;
+        (PosUpdate, WebSocket) data = ((PosUpdate, WebSocket))obj;
+        var pos = data.Item1;
+        var ws = data.Item2;
+        
+        var pos_index = posUpdates.IndexOf(pos);
+        var curPos = posUpdates[pos_index];
 
+        var mouse_x = curPos.mouse_x;
+        var mouse_y = curPos.mouse_y;
+
+        Console.WriteLine(curPos.x.ToString() + ' ' + curPos.y.ToString() + ' ' + mouse_x.ToString() + ' ' + mouse_y.ToString() + curPos.size.ToString());
         if (mouse_x!=-1 && mouse_y!=-1 && Math.Pow(curPos.x - mouse_x, 2) + Math.Pow(curPos.y - mouse_y, 2) > (curPos.size*curPos.size))
         {
             
@@ -152,10 +162,32 @@ public class HomeController : Controller
             else curPos.x -= x_norm * curPos.speed;
             if (mouse_y >= curPos.y) curPos.y += y_norm * curPos.speed;
             else curPos.y -= y_norm * curPos.speed;
-            
-            //curPos.x += (mouse_x - curPos.x) * curPos.size;
-            //curPos.y += (mouse_y - curPos.y) * curPos.size;
+
+            Console.WriteLine("изменение на: "+curPos.x.ToString() + ' ' + curPos.y.ToString());
+
+            posUpdates[pos_index].x = curPos.x;
+            posUpdates[pos_index].y = curPos.y;
+
+            Console.WriteLine("изменение на(вторая попытка): " + posUpdates[pos_index].x.ToString() + ' ' + posUpdates[pos_index].y.ToString());
+
+            await UpdateCurrentUserScreen(curPos, ws);
+
             await UpdateAll();
         }
+    }
+    async Task UpdateCurrentUserScreen(PosUpdate curPos, WebSocket ws)
+    {
+        String s = "UpdateCurrentUserScreen "+System.Text.Json.JsonSerializer.Serialize(curPos);
+        await ws.SendAsync(
+            Encoding.ASCII.GetBytes(s).ToArray(),
+            WebSocketMessageType.Text,
+            true,
+            CancellationToken.None);
+    }
+    
+    int GetRandCoord()
+    {
+        var rand = new Random();
+        return rand.Next(1000, 4000);
     }
 }
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,5 +1,23 @@
-﻿const canvas = document.createElement("canvas")
-canvas.width = 1200
+﻿const scene = document.createElement("canvas")
+scene.width = 5000
+scene.height = 5000
+const stx = scene.getContext("2d") 
+
+const circles = [];
+
+for (let i = 0; i < 200; i++) {
+    circle = {
+        x: Math.random() * scene.width,
+        y: Math.random() * scene.height,
+        r: 15,
+        color: "#00ff00"
+    };
+    //stx.arc(circle.x, circle.y, 15, 0, 2 * Math.PI)
+    circles.push(circle)
+}
+
+const canvas = document.createElement("canvas")
+canvas.width = 1500
 canvas.height = 600
 canvas.style.border = "1px solid black"
 
@@ -9,24 +27,70 @@ const gameState = {
   points: []
 }
 const colors = ["#ff0000", "#00ff00", "#0000ff"]
+let cur_x, cur_y
 
 const ws = new WebSocket("/ws")
-ws.addEventListener('message', (msg)  => {
-  const data = JSON.parse(msg.data)
-  gameState.points = data;
+
+ws.addEventListener('message', (msg) => {
+    str = String(msg.data)
+    if (str.includes("UpdateCurrentUserScreen ")) {
+        
+        str = str.replace("UpdateCurrentUserScreen ", '')
+        const data = JSON.parse(str)
+
+        cur_x = Math.max(Math.min(data.x, scene.width), 0)
+        cur_y = Math.max(Math.min(data.y, scene.height), 0)
+        size = data.size
+        gameState.points[data.user].x = cur_x
+        gameState.points[data.user].y = cur_y
+        gameState.points[data.user].size = size
+    }
+    else
+    {
+        const data = JSON.parse(str)
+        gameState.points = data;
+    }
+ 
 })
 
 function draw() {
-  // console.log(canvas)
-    ctx.fillStyle = '#ffffff'
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-  for(let point of gameState.points) {
-    console.log(point)
-    ctx.fillStyle = colors[point.user % colors.length]
-    ctx.beginPath();
-    ctx.arc(point.x, point.y, point.size, 0, 2 * 3.141592);
-    ctx.fill();
-  }
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height)  
+
+    for (let circle of circles) {
+        const screenX = circle.x - cur_x + canvas.width / 2;
+        const screenY = circle.y - cur_y + canvas.height / 2;
+
+        if (
+            screenX + circle.r > 0 &&
+            screenY + circle.r > 0 &&
+            screenX - circle.r < canvas.width &&
+            screenY - circle.r < canvas.height
+        ) {
+            ctx.beginPath();
+            ctx.arc(screenX, screenY, circle.r, 0, Math.PI * 2);
+            ctx.fillStyle = circle.color;
+            ctx.fill();
+        }
+    }
+    
+    for (let point of gameState.points)
+    {
+        const screenX = point.x - cur_x + canvas.width / 2;
+        const screenY = point.y - cur_y + canvas.height / 2;
+
+        if (
+            screenX + point.size > 0 &&
+            screenY + point.size > 0 &&
+            screenX - point.size < canvas.width &&
+            screenY - point.size < canvas.height
+        ) {
+            ctx.beginPath();
+            ctx.arc(screenX, screenY, point.size, 0, Math.PI * 2);
+            ctx.fillStyle = colors[point.user % colors.length]
+            ctx.fill();
+        }
+    }
 
   requestAnimationFrame(() => draw())
 }
@@ -34,6 +98,8 @@ draw();
 document.body.append(canvas)
 
 document.addEventListener('mousemove', e => {
-    ws.send(`move `+'X:' + e.clientX.toString()+' '+'Y:'+e.clientY.toString())
-  // console.log('keydown')
+    let data_x, data_y
+    data_x = cur_x + e.clientX - canvas.width / 2;
+    data_y = cur_y + e.clientY - canvas.height / 2;
+    ws.send(`move ` + 'X:' + data_x.toString() + ' ' + 'Y:' + data_y.toString())
 })
